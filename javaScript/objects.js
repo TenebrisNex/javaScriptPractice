d2da2efc36724c6b0fa903e572c88f148a64d50d
Cesar Soperanes <cesarsoperanes05@gmail.com>
2024-09-06 20:10:10 -0500

    Some

--- javaScript/objects.js
+++ javaScript/objects.js
@@ -1,41 +1,54 @@
+// // objects 
+
+// // collection of key value pairs 
 
 
-function createCircle(radius){
-  return {
-  radius,
-  draw(){
-    console.log('Draw');
-  }
+
+// // object oriented programming 
+
+// // const circle  = {
+// //   radius: 1, 
+// //   location : {
+// //     x: 1,
+// //     y: 1
+// //   },
+// //   inVisable: true,
+// //   draw: function(){
+// //     console.long('draw');
+// //   }
+// // };
+
+
+// function createCircle(radius){
+//   return{
+//     radius: radius,
+
+
+//   }
+
+// }
+
+// function Circle(radius){
+//   this.radius = radius;
+//   this.draw() = function(){
+//     console.log('draw');
+
+
+//   }
+//   return this;
+
+// }
+
+
+const circle ={
+  radius: 1
 };
 
+circle.color ='yellow';
+circle.draw = function(){
+
 }
-
-const circle1 = createCircle(1);
-console.log(circle1);
-
-
-
-function Circle(radius){
-  this.radius = radius;
-  this.draw = function (){
-    console.log('Draw');
-  }
-  return this 
-  
-}
-
-const circle = new Circle(1);
-
-
-const some = {
-  radius : 1
-};
-
-some.color = 'Yellow';
-some.draw = function(){};
-delete circle.color;
-delete circle.draw;
-
+delete ci
 
 
 
